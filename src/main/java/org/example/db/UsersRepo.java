e50b96c434403701baec285a558e74a9e1bfc376
19Pawel97 <19pawel9772@gmail.com>
2024-09-06 18:22:12 +0200

    Singletons of db classes implemented TODO further impl of UsersGenerator, ReservationsGenerator, CarsGenerator (filling in the DBs of user, cars and reservations)

--- src/main/java/org/example/db/UsersRepo.java
+++ src/main/java/org/example/db/UsersRepo.java
@@ -1,12 +1,24 @@
 package org.example.db;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.example.domain.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Setter
 @Getter
 public class UsersRepo {
-    List<User> users = new ArrayList<>();
+    List<User> users;
+
+    private UsersRepo(List<User> users) {
+        this.users = users;
+    }
+
+    private static UsersRepo USERS_REPO_INSTANCE = new UsersRepo(new ArrayList<>());
+
+    public static UsersRepo getInstance() {
+        return USERS_REPO_INSTANCE;
+    }
 }
