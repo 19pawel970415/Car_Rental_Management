09bf03ed561a665d9e227a65d8044d23cba96fc3
19Pawel97 <19pawel9772@gmail.com>
2024-09-06 20:41:04 +0200

    All generators added and implemented

---
+++ src/main/java/org/example/domain/ReservationGenerator.java
@@ -0,0 +1,46 @@
+package org.example.domain;
+
+import org.example.db.CarsRepo;
+import org.example.db.MarketsRepo;
+import org.example.db.ReservationsRepo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReservationGenerator {
+
+    private static final List<Market> MARKETS = MarketsRepo.getInstance().getMarkets();
+    private static final List<Car> CARS = CarsRepo.getInstance().getCars();
+
+    public static void generateReservations() {
+        Reservation reservation = new Reservation();
+        reservation.setMarket(MARKETS.get(0));
+        reservation.setCar(CARS.get(0));
+        Reservation reservation1 = new Reservation();
+        reservation.setMarket(MARKETS.get(1));
+        reservation.setCar(CARS.get(1));
+        Reservation reservation2 = new Reservation();
+        reservation.setMarket(MARKETS.get(2));
+        reservation.setCar(CARS.get(2));
+        Reservation reservation3 = new Reservation();
+        reservation.setMarket(MARKETS.get(3));
+        reservation.setCar(CARS.get(3));
+        Reservation reservation4 = new Reservation();
+        reservation.setMarket(MARKETS.get(0));
+        reservation.setCar(CARS.get(4));
+        Reservation reservation5 = new Reservation();
+        reservation.setMarket(MARKETS.get(1));
+        reservation.setCar(CARS.get(5));
+
+        ReservationsRepo.getInstance().setReservations(
+                Arrays.asList(
+                        reservation,
+                        reservation1,
+                        reservation2,
+                        reservation3,
+                        reservation4,
+                        reservation5
+                )
+        );
+    }
+}
--- src/main/java/org/example/domain/UserGenerator.java
+++ src/main/java/org/example/domain/UserGenerator.java
@@ -3,12 +3,56 @@ package org.example.domain;
 import org.example.db.ReservationsRepo;
 import org.example.db.UsersRepo;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class UserGenerator {
 
-    public static final UsersRepo USER_REPO = new UsersRepo();
-    public static final ReservationsRepo RESERVATION_REPO = new ReservationsRepo();
+    private static final List<Reservation> RESERVATIONS = ReservationsRepo.getInstance().getReservations();
 
     public static void generateUsers() {
+        User user = User.builder()
+                .name("James")
+                .surname("Bond")
+                .email("jamesBond@gmail.com")
+                .login("JamesBond")
+                .password("jamesbond007")
+                .reservations(Arrays.asList(RESERVATIONS.get(0), RESERVATIONS.get(1)))
+                .build();
 
+        ReservationsRepo.getInstance().getReservations().get(0).setUser(user);
+        ReservationsRepo.getInstance().getReservations().get(1).setUser(user);
+
+        User user1 = User.builder()
+                .name("Ethan")
+                .surname("Hunt")
+                .email("ethanHunt@gmail.com")
+                .login("EthanHunt")
+                .password("missionimpossible")
+                .reservations(Arrays.asList(RESERVATIONS.get(2), RESERVATIONS.get(3)))
+                .build();
+
+        ReservationsRepo.getInstance().getReservations().get(2).setUser(user1);
+        ReservationsRepo.getInstance().getReservations().get(3).setUser(user1);
+
+        User user2 = User.builder()
+                .name("Jason")
+                .surname("Bourne")
+                .email("jasonBourne@gmail.com")
+                .login("JasonBourne")
+                .password("bourneidentity")
+                .reservations(Arrays.asList(RESERVATIONS.get(4), RESERVATIONS.get(5)))
+                .build();
+
+        ReservationsRepo.getInstance().getReservations().get(4).setUser(user2);
+        ReservationsRepo.getInstance().getReservations().get(5).setUser(user2);
+
+        UsersRepo.getInstance().setUsers(
+                Arrays.asList(
+                        user,
+                        user1,
+                        user2
+                )
+        );
     }
 }
