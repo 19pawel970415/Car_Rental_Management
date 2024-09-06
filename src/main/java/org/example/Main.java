305bc337bf1f2526a556fcc0087594d8c0d82092
19Pawel97 <19pawel9772@gmail.com>
2024-09-06 21:25:13 +0200

    ...AND DUSTED

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,5 @@
 package org.example;
 
-import org.example.db.CarsRepo;
-import org.example.db.MarketsRepo;
-import org.example.db.ReservationsRepo;
-import org.example.db.UsersRepo;
 import org.example.domain.CarsGenerator;
 import org.example.domain.MarketsGenerator;
 import org.example.domain.ReservationsGenerator;
