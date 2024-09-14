fce76a7f884eb0e7b5f1e6a5c0f756564402c959
19Pawel97 <19pawel9772@gmail.com>
2024-09-14 20:23:43 +0200

    Structural patters init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,5 +11,14 @@ public class Main {
         MarketsGenerator.generateMarkets();
         ReservationsGenerator.generateReservations();
         UsersGenerator.generateUsers();
+        //TODO
+        /*
+        Expand the existing car reservation management system by implementing the Adapter pattern
+        to integrate with a new external library for the payment system.
+        Create a PaymentProcessor interface, which will have a method processPayment(Order order).
+        Then, create a class ExternalPaymentSystem, which has its own payment methods,
+        but their signatures differ from your interface.
+        Create an adapter, ExternalPaymentAdapter, which will adapt the external payment system to your interface.
+         */
     }
 }
